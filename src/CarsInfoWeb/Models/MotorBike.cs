c041876d3ca96195ae380d68dc72800c1a975009
Nikola Spirov <n_spirov@abv.bg>
2017-04-14 23:52:50 +0300

    Added Models folder with Classes for Vehicle,Car,MotorBike,User and Advertisement.  Last two could be deleted in the future.

---
+++ src/CarsInfoWeb/Models/MotorBike.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace CarsInfoWeb.Models
+{
+    public class MotorBike:Vehicle
+    {
+        private int CubicCapacity { get; set; }
+        private string Type { get; set; }
+
+        public MotorBike(string brand, string model, int capacity, string type,decimal price)
+        {
+            //this.Id = Guid.NewGuid();
+            this.Brand = brand;
+            this.Model = model;
+            this.CubicCapacity = capacity;
+            this.Type = type;
+            this.Price = price;
+        }
+    }
+}
