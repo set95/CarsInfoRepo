b7cc4095882581ebc00b6431caff976372d4a27d
Nikola Spirov <n_spirov@abv.bg>
2017-04-30 13:12:33 +0300

    added AccountControler

---
+++ src/CarsInfoWeb/Controllers/AccountControllercs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsInfoWeb.Controllers
+{
+    public class AccountControllercs
+    {
+    }
+}
