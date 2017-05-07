88082129c158ba50075a0aaf7a7401eb3f9c0439
preskomilev <preslav.k.milev@gmail.com>
2017-05-07 13:25:40 +0300

    Regular commit.

--- src/CarsInfoWeb/Models/CarModel.cs
+++ src/CarsInfoWeb/Models/CarModel.cs
@@ -6,32 +6,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CarsInfoWeb.Models
 {
-    public class BloggingContext : DbContext
-    {
-        public BloggingContext(DbContextOptions<BloggingContext> options)
-            : base(options)
-        { }
-
-        public DbSet<Cars> Blogs { get; set; }
-        public DbSet<Post> Posts { get; set; }
-    }
 
     public class Cars
     {
         public int CarId { get; set; }
+        public string Style { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
+        public string Year { get; set; }
+        public string Engine { get; set; }
+        public string Transmission { get; set; }
 
-        public List<Post> Posts { get; set; }
     }
 
-    public class Post
-    {
-        public int PostId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-
-        public int BlogId { get; set; }
-        public Cars Cars { get; set; }
-    }
+   
 }
